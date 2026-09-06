86f0f2064399eb6822805a82d1f4acec58b3e4c4
unknown <padmakarnarwade05@gmail.com>
2026-09-06 10:56:08 +0530

    add log for token is provided or not to show

--- middleware/authmiddleware.js
+++ middleware/authmiddleware.js
@@ -2,6 +2,8 @@ import jwt from 'jsonwebtoken';
 
 const verifyToken = (req, res, next) => {
    const token=req.cookies.token;
+  
+    console.log("🍪 Cookie token:", token ? "TOKEN FOUND" : "TOKEN NOT FOUND");
    //const token='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyaWQiOjEsInVzZXJuYW1lIjoiUGFkbWFrYXIgTmFyd2FkZSIsInJvbGVpZCI6bnVsbCwiaWF0IjoxNzY4NTc3NDI1LCJleHAiOjE3Njg1ODEwMjV9.OsDGVz1AObwC9OIMvIcLmK93qIRW22w98n1nBVaWTOY';
    if (!token) {
     return res.status(401).json({ message: 'No token provided' });
